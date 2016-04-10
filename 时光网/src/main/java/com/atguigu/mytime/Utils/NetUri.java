e3ca5a728a71a87383757c6f7de252108b712705
lzzcg66 <13001329701@163.com>
2016-04-10 16:34:25 +0800

    梁真真 sx

---
+++ 时光网/src/main/java/com/atguigu/mytime/Utils/NetUri.java
@@ -0,0 +1,13 @@
+package com.atguigu.mytime.Utils;
+
+/**
+ * 联网请求地址
+ * Created by Administrator on 16-4-8.
+ */
+public class NetUri {
+    public static final String CITY_URL="http://api.m.mtime.cn/Showtime/HotCitiesByCinema.api";
+    public static final String DISCOVER_LIST = "http://api.m.mtime.cn/PageSubArea/TrailerList.api";
+    public static final String DISCOVER_TOP = "http://api.m.mtime.cn/PageSubArea/GetRecommendationIndexInfo.api";
+    public static final String COMMENT_LIST = "http://api.m.mtime.cn/MobileMovie/Review.api?needTop=false";
+
+}
