57ae58220abeef086eea86be598b08f0f1bdc7ca
00zifeng00 <w840750145@qq.com>
2016-04-19 11:26:59 +0800

    万能播放器

--- 时光网/src/main/java/com/atguigu/mytime/discoverpagers/Newspager.java
+++ 时光网/src/main/java/com/atguigu/mytime/discoverpagers/Newspager.java
@@ -1,44 +1,266 @@
 package com.atguigu.mytime.discoverpagers;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.view.Gravity;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.atguigu.mytime.R;
+import com.atguigu.mytime.Utils.NetUri;
+import com.atguigu.mytime.Utils.SpUtils;
 import com.atguigu.mytime.base.BasePager;
+import com.atguigu.mytime.bean.NewsInfo;
+import com.atguigu.refreshlistview.RefreshListView;
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import org.json.JSONObject;
 
+import java.util.List;
+
+import okhttp3.Call;
+
 /**
+ * 新闻
  * Created by Administrator on 16-4-8.
  */
 public class Newspager extends BasePager {
-    private TextView textView;
-    private JSONObject topNews;
+    private JSONObject topNews;//头部信息
+    private RefreshListView listview_newspager;
+    //头布局
+    private ImageView prevue_head_icon;
+    private TextView headview_title;
+    private ImageView image_top_picket;
+    private ImageView image_top_chinese;
+    //列表信息
+    private int totalCount;
+    private int pageCount;
+    private List<NewsInfo.NewsListEntity> newsList;
+    private NewsAdapter adapter;
+
     public Newspager(Activity mactivity, JSONObject topNews) {
         super(mactivity);
-        this.topNews=topNews;
+        this.topNews = topNews;
 
     }
 
     /**
      * 初始化视图
+     *
      * @return
      */
     @Override
     public View initView() {
-        textView = new TextView(mactivity);
-        textView.setTextSize(30);
-        textView.setTextColor(Color.BLACK);
-        textView.setGravity(Gravity.CENTER);
-        //initData();
-        return textView;
+        View view = View.inflate(mactivity, R.layout.layout_newspager, null);
+        listview_newspager = (RefreshListView) view.findViewById(R.id.listview_newspager);
+        //加载头布局
+        View headView = View.inflate(mactivity, R.layout.news_head, null);
+        prevue_head_icon = (ImageView) headView.findViewById(R.id.prevue_head_icon);
+        headview_title = (TextView) headView.findViewById(R.id.headview_title);
+        image_top_picket = (ImageView) headView.findViewById(R.id.image_top_picket);
+        image_top_chinese = (ImageView) headView.findViewById(R.id.image_top_chinese);
+        //给listview添加头布局
+        listview_newspager.addTopNewsView(headView);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("新闻");
+        //初始化头部数据
+        Glide.with(mactivity).load(topNews.optString("imageUrl")).into(prevue_head_icon);
+        headview_title.setText(topNews.optString("title"));
+        //从本地获取数据
+        //从本地获取图片
+        String savejson = SpUtils.getInitialize(mactivity).getJsonData("news_list");
+        if (!TextUtils.isEmpty(savejson)) {
+            processData(savejson);
+        }
+        //联网请求数据
+        getDataFromnet();
+
+    }
+
+    /**
+     * 联网请求数据
+     */
+    private void getDataFromnet() {
+        String uri = NetUri.NEWSPAGER_LIST;
+        OkHttpUtils.get().url(uri).build().execute(new NewCallback());
+    }
+
+    class NewCallback extends StringCallback {
+
+
+        @Override
+        public void onError(Call call, Exception e) {
+
+        }
+
+        @Override
+        public void onResponse(String response) {
+            //请求数据成功解析数据
+            processData(response);
+            //保存到本地
+            SpUtils.getInitialize(mactivity).saveJson("news_list", response);
+        }
+    }
+
+    /**
+     * 解析数据
+     *
+     * @param response
+     */
+    private void processData(String response) {
+        parseData(response);//解析数据成功
+        //设置adapter
+        adapter = new NewsAdapter();
+        //装配数据
+        listview_newspager.setAdapter(adapter);
+
+    }
+
+    private void parseData(String response) {
+        NewsInfo newsInfo = new Gson().fromJson(response, NewsInfo.class);
+        //总的页数和条数
+        totalCount = newsInfo.getTotalCount();
+        pageCount = newsInfo.getPageCount();
+        //item的数据集合
+        newsList = newsInfo.getNewsList();
+    }
+
+    class NewsAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return newsList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            NewsInfo.NewsListEntity itemInfo = newsList.get(position);
+            List<NewsInfo.NewsListEntity.ImagesEntity> images = itemInfo.getImages();
+            int type = itemInfo.getType();
+
+            return type;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 3;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            NewsInfo.NewsListEntity itemInfo = newsList.get(position);
+            List<NewsInfo.NewsListEntity.ImagesEntity> images = itemInfo.getImages();
+            int type = itemInfo.getType();
+            ViewHolder holder = null;
+            //分类型的listView
+            if (convertView == null){
+                holder = new ViewHolder();
+                switch (type) {
+                    case 0:
+                        convertView = View.inflate(mactivity,R.layout.layout_item_type0,null);
+                        holder.item_icon1=(ImageView)convertView.findViewById(R.id.item_icon1);
+                        holder.tv_title1 = (TextView) convertView.findViewById(R.id.tv_title1);
+                        holder.tv_desc1 =(TextView)convertView.findViewById(R.id.tv_desc1);
+                        holder.tv_comment1 = (TextView)convertView.findViewById(R.id.tv_comment1);
+                        break;
+                    case 1:
+                        convertView = View.inflate(mactivity, R.layout.layout_item_type1,null);
+                        holder.text_title0 = (TextView) convertView.findViewById(R.id.text_title0);
+//                        holder.text_time0 = (TextView) convertView.findViewById(R.id.text_time0);
+                        holder.tv_comment0 = (TextView) convertView.findViewById(R.id.tv_comment0);
+                        holder.image3 = (ImageView) convertView.findViewById(R.id.image3);
+                        holder.image1 = (ImageView) convertView.findViewById(R.id.image1);
+                        holder.image2 = (ImageView) convertView.findViewById(R.id.image2);
+                        if(images.size()==3) {
+                            holder.image3.setVisibility(View.VISIBLE);
+                        }else{
+                            holder.image3.setVisibility(View.GONE);
+                        }
+
+                        break;
+                    case 2:
+                        convertView = View.inflate(mactivity, R.layout.layout_item_type2, null);
+                        holder.item2_icon = (ImageView) convertView.findViewById(R.id.item2_icon);
+                        holder.tv_title2 = (TextView) convertView.findViewById(R.id.tv_title2);
+                        holder.tv_desc2 = (TextView) convertView.findViewById(R.id.tv_desc2);
+                        holder.tv_comment2 = (TextView) convertView.findViewById(R.id.tv_comment2);
+                        break;
+                    default:
+                        convertView = View.inflate(mactivity, R.layout.layout_item_type_default, null);
+                        holder.iv_default = (ImageView) convertView.findViewById(R.id.iv_default);
+                        break;
+                }
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            switch (type) {
+                case 0 :
+                    Glide.with(mactivity).load(itemInfo.getImage()).into(holder.item_icon1);
+                    holder.tv_title1.setText(itemInfo.getTitle());
+                    holder.tv_desc1.setText(itemInfo.getTitle2());
+                    holder.tv_comment1.setText("评论" + itemInfo.getCommentCount());
+                    break;
+                case 1 :
+                    if(images.size()==3) {
+                        Glide.with(mactivity).load(images.get(2).getUrl1()).into(holder.image3);
+                    }
+                    Glide.with(mactivity).load(images.get(0).getUrl1()).into(holder.image1);
+                    Glide.with(mactivity).load(images.get(1).getUrl1()).into(holder.image2);
+                    holder.text_title0.setText(itemInfo.getTitle());
+                    holder.tv_comment0.setText("评论" + itemInfo.getCommentCount());
+                    break;
+                case 2 :
+                    holder.tv_title2.setText(itemInfo.getTitle());
+                    holder.tv_desc2.setText(itemInfo.getTitle2());
+                    holder.tv_comment2.setText("评论" + itemInfo.getCommentCount());
+                    Glide.with(mactivity).load(itemInfo.getImage()).into(holder.item2_icon);
+                    break;
+                default:
+                    Glide.with(mactivity).load(itemInfo.getImage()).into(holder.iv_default);
+                    break;
+            }
+
+            return convertView;
+        }
+
+        class ViewHolder {
+            ImageView iv_default;
+            TextView text_title0;
+            TextView text_time0;
+            TextView tv_comment0;
+            ImageView image1;
+            ImageView image2;
+            ImageView image3;
+            ImageView item_icon1;
+            TextView tv_title1;
+            TextView tv_desc1;
+            TextView tv_comment1;
+            ImageView item2_icon;
+            TextView tv_title2;
+            TextView tv_desc2;
+            TextView tv_comment2;
+
+        }
     }
 }
--- 时光网/src/main/java/com/atguigu/mytime/discoverpagers/Ranklistpager.java
+++ 时光网/src/main/java/com/atguigu/mytime/discoverpagers/Ranklistpager.java
@@ -1,21 +1,57 @@
 package com.atguigu.mytime.discoverpagers;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.atguigu.mytime.R;
+import com.atguigu.mytime.Utils.MessageUtils;
+import com.atguigu.mytime.Utils.NetUri;
+import com.atguigu.mytime.Utils.SpUtils;
+import com.atguigu.mytime.adapter.RanklistAdapter;
 import com.atguigu.mytime.base.BasePager;
 import com.atguigu.refreshlistview.RefreshListView;
+import com.bumptech.glide.Glide;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
 
 /**
  * 排行榜
  * Created by Administrator on 16-4-8.
  */
-public class Ranklistpager extends BasePager {
+public class Ranklistpager extends BasePager implements AdapterView.OnItemClickListener {
 private RefreshListView listview_ranklist;
-    private JSONObject topList ;
+    private ImageView loading;
+    //帧动画
+    private AnimationDrawable animationDrawable;
+    private ImageView loading_bg;
+    private ImageView loading_failed;
+    private ImageView image_top_pick;
+    private RanklistAdapter adapter;
+    private ImageView prevue_head_icon;
+    private TextView headview_title;
+    private JSONObject topList;
+    private List<JSONObject> lists;
+    private ImageView image_top_time;
+    private ImageView image_top_chinese;
+    private int totalCount;//list中的item的总条数
+    private int pageCount;//item的页数
+    private boolean isLoadMore;
+    private int i=1;
+
     public Ranklistpager(Activity mactivity, JSONObject topList) {
         super(mactivity);
         this.topList=topList;
@@ -25,12 +61,204 @@ private RefreshListView listview_ranklist;
     public View initView() {
         View view = View.inflate(mactivity, R.layout.listview_ranklist, null);
         listview_ranklist = (RefreshListView) view.findViewById(R.id.listview_ranklist);
+        loading = (ImageView) view.findViewById(R.id.loading);
+        loading_bg = (ImageView) view.findViewById(R.id.loading_bg);
+        loading_failed = (ImageView) view.findViewById(R.id.loading_failed);
+        //设置监听
+        loading_failed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loading_bg.setVisibility(View.VISIBLE);
+                loading_failed.setVisibility(View.GONE);
+                loading.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+        /**
+         *获取帧动画
+         */
+         animationDrawable = (AnimationDrawable) loading.getBackground();
 
+        View headView = View.inflate(mactivity, R.layout.ranklist_headview, null);
+        prevue_head_icon = (ImageView) headView.findViewById(R.id.prevue_head_icon);
+        headview_title = (TextView) headView.findViewById(R.id.headview_title);
+        image_top_time = (ImageView) headView.findViewById(R.id.image_top_time);
+        image_top_chinese = (ImageView) headView.findViewById(R.id.image_top_chinese);
+        image_top_pick = (ImageView) headView.findViewById(R.id.image_top_pick);
+
+        //给listView添加布局
+        listview_ranklist.addTopNewsView(headView);
+        //设置下拉刷新和上拉加载更多的监听
+        listview_ranklist.setOnRefreshListener(new MyOnRefreshListener());
+        //给listView设置点击事件的监听
+        listview_ranklist.setOnItemClickListener(this);
         return view;
     }
+
+    /**
+     * 点击某个item的回调方法
+     * @param parent
+     * @param view
+     * @param position
+     * @param id
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //点击的时候进入详细界面
+    }
+
+    class MyOnRefreshListener implements RefreshListView.OnRefreshListener{
+
+    @Override
+    public void onPullDownRefresh() {
+        //下拉刷新:使用的是新的adapter
+        getDatafromNet();
+    }
+
+    @Override
+    public void onLoadMore() {
+        //上拉加载更多：使用的是同一个adapter，只需要更新adapter
+        if(lists.size()<totalCount) {
+            isLoadMore=true;
+            getDatafromNet();
+
+        }else{
+            isLoadMore=false;
+            //如果加载更多的时候，没有数据了，也恢复到原来的状态
+            listview_ranklist.onFinishRefresh(false);
+        }
+
+    }
+
+
+}
+
 
     @Override
     public void initData() {
         super.initData();
+        //初始化头部
+        initHeadView();
+        //从本地获取图片
+        String savejson=SpUtils.getInitialize(mactivity).getJsonData("neturi_ranklist");
+        if(!TextUtils.isEmpty(savejson)) {
+            processJson(savejson);
+        }
+        //联网请求数据
+        getDatafromNet();
+    }
+
+    /**
+     * 初始化头部
+     */
+    private void initHeadView() {
+        Glide.with(mactivity)
+                .load(topList.optString("imageUrl"))
+                .into(prevue_head_icon);
+        headview_title.setText(topList.optString("title"));
+
+    }
+
+    /**
+     * 联网请求数据
+     */
+    private void getDatafromNet() {
+        loading_bg.setVisibility(View.VISIBLE);
+        loading.setVisibility(View.VISIBLE);
+        //开启帧动画
+        animationDrawable.start();
+
+        String uri = "";
+        if (isLoadMore) {
+           i++;
+            uri = NetUri.RANKLIST_BASE+i;
+        }else{
+
+            uri = NetUri.RANKLIST_BASE+1;
+        }
+        OkHttpUtils.get().url(uri)
+                .build()
+                .execute(new MyCallback());
+
+    }
+    class MyCallback extends StringCallback {
+
+        @Override
+        public void onError(Call call, Exception e) {
+
+            MessageUtils.showMessage(mactivity, "联网请求数据失败");
+            animationDrawable.stop();
+            //显示加载失败的图片
+            loading.setVisibility(View.GONE);
+            loading_bg.setVisibility(View.GONE);
+            loading_failed.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onResponse(String response) {
+            loading_failed.setVisibility(View.GONE);
+            loading.setVisibility(View.GONE);
+            loading_bg.setVisibility(View.GONE);
+            if(isLoadMore) {
+                processJson(response);
+                //请求数据成功，恢复到原来的状态
+                listview_ranklist.onFinishRefresh(true);
+            }else{
+                //解析数据
+                processJson(response);
+                //保存数据到本地
+                SpUtils.getInitialize(mactivity).saveJson("neturi_ranklist",response);
+            }
+        }
+    }
+
+    /**
+     * 解析数据
+     * @param response
+     */
+    public void processJson(String response) {
+        parseJson(response);//解析数据成功
+        //设置adapter
+        if(isLoadMore&&adapter!=null) {
+            adapter.notifyDataSetChanged();
+            //加载数据的时候，将item定位到最后一个
+            listview_ranklist.setSelection(listview_ranklist.getLastVisiblePosition());
+        }else{
+
+            adapter=new RanklistAdapter(mactivity,lists);
+            listview_ranklist.setSelection(listview_ranklist.getFirstVisiblePosition());
+            //显示列表
+            listview_ranklist.setAdapter(adapter);
+        }
+
+
+    }
+
+    private void parseJson(String response) {
+        if(lists==null) {
+
+            lists = new ArrayList<>();
+        }
+        if(!isLoadMore) {
+            lists.clear();
+        }
+
+        //手动解析数据
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            //获取共有多条数据
+            totalCount=jsonObject.optInt("totalCount");
+            pageCount=jsonObject.optInt("pageCount");
+            JSONArray topLists = jsonObject.optJSONArray("topLists");
+            //遍历数组
+            for (int i = 0; i < topLists.length(); i++) {
+                JSONObject item = (JSONObject) topLists.get(i);
+                //添加到集合中
+                lists.add(item);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
